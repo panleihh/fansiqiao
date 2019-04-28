c0699f8c108e6c329ae77f816874ea19128b3c3f
panlei <panlei@fangdd.com>
2019-04-28 19:29:05 +0800

    feat: add BMap

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import { BMAP_ANIMATION_BOUNCE } from '../common/bMap_data';
+
 import styles from './Contact.module.css';
 
 const list = [
@@ -10,6 +12,35 @@ const list = [
 
 export default class Contact extends Component {
 
+  componentDidMount() {
+    const BMap = window.BMap;
+    const map = new BMap.Map("allmap"); // 创建Map实例
+    const point = new BMap.Point(116.496253, 39.97696);
+
+    map.centerAndZoom(point, 12); // 初始化地图,设置中心点坐标和地图级别
+    map.setCurrentCity("北京"); // 设置地图显示的城市 此项是必须设置的
+    map.enableScrollWheelZoom(true); //开启鼠标滚轮缩放
+    map.addControl(new BMap.ScaleControl()); //添加比例尺控件
+
+    // const Icon = new BMap.Icon("http://lbsyun.baidu.com/jsdemo/img/fox.gif", new BMap.Size(100, 100) );  //设置标注的图标
+    // const marker = new BMap.Marker(new BMap.Point(116.496253, 39.97696), { icon: Icon });  //设置标注的经纬度
+    const marker = new BMap.Marker(point);
+    map.addOverlay(marker); //把标注添加到地图上
+    marker.setAnimation(BMAP_ANIMATION_BOUNCE); //跳动的动画
+
+    const sContent = "朝阳区将台路";
+    var infoWindow = new BMap.InfoWindow(sContent);  // 创建信息窗口对象
+    // map.openInfoWindow(infoWindow, point); //开启信息窗口
+
+    marker.addEventListener("click", function () {
+      this.openInfoWindow(infoWindow);
+      //图片加载完毕重绘infowindow
+      // document.getElementById('imgDemo').onload = function (){
+      //   infoWindow.redraw();   //防止在网速较慢，图片未加载时，生成的信息框高度比图片的总高度小，导致图片部分被隐藏
+      // }
+    });
+  }
+
   render() {
 
     return (
@@ -19,7 +50,7 @@ export default class Contact extends Component {
         </h2>
 
         <div className={styles.map}>
-          map
+          <div id={"allmap"} style={{ width: "100%", height: "100%" }} />
         </div>
 
         <ul className={styles.list}>
